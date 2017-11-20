6bd091cd44d47df5c5c7f3aac83b3e58299affc9
FC84 <FTChu5@gmail.com>
2017-11-19 23:35:58 -0800

    updated_version

---
+++ app/data/friends.js
@@ -0,0 +1,16 @@
+var friendList = [ {
+	"name" : "Mitsuha",
+	"photo" : "https://i.pinimg.com/originals/8a/2e/fa/8a2efa9740e9249e7e26ea650542b807.jpg",
+	"scores" : [5,5,2,3,1,5,2,1,3,5]
+}, {
+	"name" : "Kirito",
+	"photo" : "https://s1.narvii.com/image/7xu6jlu6ysrh6ocu374dxrzigsjps6m6_hq.jpg",
+	"scores" : [1,5,1,1,1,1,1,1,5,5]
+}, {
+	"name" : "Kaori",
+	"photo" : "http://images6.fanpop.com/image/photos/39600000/Kaori-Miyazono-shigatsu-wa-kimi-no-uso-39652880-580-580.jpg",
+	"scores" : [5,3,2,3,2,5,1,3,3,4]
+}];
+
+
+module.exports = friendList;
